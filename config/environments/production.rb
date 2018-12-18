db158bb5f388cec870da748ee6c5662831939d92
anand2905 <anand29.ku@gmail.com>
2018-12-18 23:39:31 +0530

    add email with sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,9 @@ Rails.application.configure do
   # `config/secrets.yml.key`.
   config.read_encrypted_secrets = true
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => ' https://anand-saas-app1.herokuapp.com', :protocol => 'https'}
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
